69389c4575023a00ad6b7852f13b21df6b3cee42
Junior Ramos <ramosjr729@gmail.com>
2025-12-16 13:36:51 -0300

    Create index.js

---
+++ inicio/index.js
@@ -0,0 +1,10 @@
+let numero = 8
+let contador = 0;
+
+console.log("a tabuada de multiplicação n°: " +numero);
+
+while (contador <= 10) {
+    let resultado = numero * contador;
+    console.log(numero + "x" + contador+ "=" +resultado);
+    contador++;
+}
